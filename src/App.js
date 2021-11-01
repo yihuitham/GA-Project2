b6fe3518e898e7b035204f4918d279030e8bddb6
YihuiTham <yihuitham@gmail.com>
2021-11-01 21:57:52 +0800

    main page done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Typography,
+  Button,
+  Box,
+  Grid,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Link } from "react-router-dom";
+import { white } from "material-ui-colors";
+
+const image =
+  "https://static.travelworks.de/media/header/work-and-travel/work-and-travel-header_01.jpg";
+
+const useStyles = makeStyles({
+  mainContent: {
+    minHeight: "100vh",
+    textAlign: "center",
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+});
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          color: "darkred",
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+          "& h1": {
+            color: "black",
+          },
+        },
+      },
+    },
+  },
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#FFFFFF",
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box className={classes.mainContent}>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          padding={5}
+          backgroundColor="rgba(255,255,255, 0.5)"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid item>
+            <Typography
+              variant="h2"
+              component="h1"
+              style={{ fontFamily: "Bungee Inline, cursive" }}
+            >
+              Thinking of going somewhere?
+            </Typography>
+          </Grid>
+          <Grid item p={2}>
+            <Button
+              variant="contained"
+              size="large"
+              component={Link}
+              to="/about"
+              style={{ fontFamily: "Bungee Inline, cursive" }}
+            >
+              Let's get you ready!
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
+    // </div>
   );
 }
 
 export default App;
+
+// href="#contained-buttons"
