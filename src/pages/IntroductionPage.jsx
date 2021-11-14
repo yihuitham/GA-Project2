759b9d64206e5c0c3568031a4ae39515d8d99774
YihuiTham <yihuitham@gmail.com>
2021-11-14 12:15:54 +0800

    change intro image

--- src/pages/IntroductionPage.jsx
+++ src/pages/IntroductionPage.jsx
@@ -13,15 +13,18 @@ import { blueGrey } from "@mui/material/colors";
 import { makeStyles } from "@mui/styles";
 import { Link } from "react-router-dom";
 
+// const image =
+//   "https://static.travelworks.de/media/header/work-and-travel/work-and-travel-header_01.jpg";
+
 const image =
-  "https://static.travelworks.de/media/header/work-and-travel/work-and-travel-header_01.jpg";
+  "https://media.istockphoto.com/vectors/happy-family-travelling-by-car-vector-id1280010427?k=20&m=1280010427&s=612x612&w=0&h=TiC_ki6e-3dwMgPw2u-ww9oNCoL9O7nV-rJuTed2p3w=";
 
 const useStyles = makeStyles((theme) => ({
   mainContent: {
     minHeight: "100vh",
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center",
+    // justifyContent: "center",
     alignContent: "center",
     textAlign: "center",
   },
@@ -62,10 +65,10 @@ function IntroductionPage() {
           direction="column"
           pt={5}
           pb={5}
-          backgroundColor="rgba(255,255,255, 0.5)"
+          // backgroundColor="rgba(255,255,255, 0.5)"
         >
-          <Grid item>
-            <Typography variant="h2">Planning to travel somewhere?</Typography>
+          <Grid item sx={{ px: 5, pt: 6 }}>
+            <Typography variant="h1">Going on an adventure?</Typography>
           </Grid>
           <Grid item p={2}>
             <Button
